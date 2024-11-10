2472121a51e27e916af0bf968868da0697627265
RossDmello2 <rossdmello869@gmail.com>
2024-11-10 16:55:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import UserProfile from './UserProfile';
+import React, { Component } from 'react';
+import Toggle from './Toggle';
 
-class App extends React.Component {
+class App extends Component {
+  handleToggle = () => {
+    console.log("Toggle clicked!");
+  };
+
   render() {
     return (
       <div>
-        <h1>User Profiles</h1>
-        <UserProfile name="Alice" age={25} location="New York" />
-        <UserProfile name="Bob" age={30} location="Los Angeles" />
-        <UserProfile name="Charlie" age={35} location="Chicago" />
-        {/* This will use default props */}
-        <UserProfile />
+        <h1>Toggle Button</h1>
+        <Toggle handleToggle={this.handleToggle} />
       </div>
     );
   }
